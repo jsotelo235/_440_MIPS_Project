374f307cec7892fe970ede7adc9bfe54f9c92a6f
Jose Pacheco <pacheco216@gmail.com>
2018-02-27 10:49:12 -0800

    Fixed indentation

--- Lab2/ALU.v
+++ Lab2/ALU.v
@@ -1,31 +1,32 @@
 `timescale 1ns / 1ps
-//*****************************************************************//
-//  This document contains information proprietary to the		    //
-//  CSULB student that created the file - any reuse without		    //
-//  adequate approval and documentation is prohibited				    //
-//  																					 //
-//  Class: CECS 440																 //
-//  Project name: Lab 2 - ALU and ALU Control Modules					 //
-//  File name: ALU.v															    //
-// 																				    //
-//  Created by Jose Pacheco on February 27, 2018						 //
-//  Copyright © 2018 Jose Pacheco. All rights reserved.				 //
-//  																					 //
-//  Abstract: 32-bit Arithmetic Logic Unit used to perform various //
-//  calculations on 32-bit integers. There are currently 6		    //
-//  operations to be performed.												 //
-//																						 //
-//																						 //
-//  In submitting this file for class work at CSULB                //
-//  I am confirming that this is my work and the work              //
-//  of no one else.																 //
-//																						 //
-//  In the event other code sources are utilized I will            //
-//  document which portion of code and who is the author           //
-//																						 //
-//  In submitting this code I acknowledge that plagiarism          //
-//  in student project work is subject to dismissal from the class //
-//*****************************************************************//
+//*******************************************************//
+// This document contains information proprietary        //
+// to the CSULB student that created the                 //
+// file - any reuse without adequate approval and        //
+// documentation is prohibited                           //
+//                                                       //
+// Class:      CECS 440                                  //
+// Project:    Lab 2 - ALU and ALU Control Modules       //
+// File name:  ALU.v                                     //
+//                                                       //
+// Created by Jose Pacheco on February 27, 2018          //
+// Copyright © 2018 Jose Pacheco. All rights reserved.   //
+//                                                       //
+// Abstract: 32-bit Arithmetic Logic Unit used to        //
+// perform various calculations on 32-bit integers.      //
+// There are currently 6 operations to be performed.     //
+//                                                       //
+// In submitting this file for class work at CSULB       //
+// I am confirming that this is my work and the work     //
+// of no one else.                                       // 
+//                                                       //
+// In the event other code source are utilized I will    //
+// document which portion of code and who is the author  //
+//                                                       //
+// In submitting this code I acknowledge that plagiarism //
+// in student project work is subject to dismissal from  //
+// the class                                             //
+//*******************************************************//
 module ALU(A, B, ALU_Ctl, Zero_Flag, ALU_result);
 	
 	input [31:0]  A;
